cc9104109312dd19ff2baa9f42dfb33a73503cdf
Chris Cundill <chrisc@diligentsoft.co>
2024-03-27 16:21:35 +0000

    Removed original request-processor which is no longer used and removed from docker-compose file.  Also removed sqs-test variant of docker-compose file which is no longer needed.

--- request-processor/src/crud.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import Session
-
-from request_model import models
-
-
-def get_request(db: Session, request_id: int):
-    return db.query(models.Request).filter(models.Request.id == request_id).first()
