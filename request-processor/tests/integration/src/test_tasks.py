4a2de17bf29d629cb6ef30eb5b964b9d51b6ddea
Feba <feba.rajan@levellingup.gov.uk>
2024-07-09 09:15:44 +0100

    Parameterized rp/tests/integ/src/test_tasks. Need to update request-processor to use new inputs for get_resource_unidentified_lookups from digital-land-python.

--- request-processor/tests/integration/src/test_tasks.py
+++ request-processor/tests/integration/src/test_tasks.py
@@ -14,6 +14,10 @@ def test_data_dir(test_dir):
     return os.path.realpath(f"{test_dir}/../../data")
 
 
+@pytest.mark.parametrize("filename, uploaded_filename, expected_status",
+                         [("article-direction-area.csv", "492f15d8-45e4-427e-bde0-f60d69889f40", "COMPLETE"),
+                          ("invalid.csv", "invalid", "FAILED")]
+                         )
 def test_check_datafile(
     mocker,
     celery_app,
@@ -23,9 +27,12 @@ def test_check_datafile(
     mock_directories,
     mock_fetch_pipeline_csvs,
     test_data_dir,
+    filename,
+    uploaded_filename,
+    expected_status
 ):
     """
-    This function tests the check_datafile task for an valid file validation.
+    This function tests the check_datafile task for file validation.
 
     Args:
         mocker: Mocking framework for Python.
@@ -36,6 +43,9 @@ def test_check_datafile(
         mock_directories: Mocked directories.
         mock_fetch_pipeline_csvs: Mock function for fetching pipeline CSVs.
         test_data_dir: Directory containing test data.
+        filename: Name of the file to validate.
+        uploaded_filename: Uploaded filename.
+        expected_status: Expected status after validation.
     """
     request_model = models.Request(
         type=schemas.RequestTypeEnum.check_file,
@@ -45,8 +55,8 @@ def test_check_datafile(
         params=schemas.CheckFileParams(
             collection="article-4-direction",
             dataset="article-4-direction-area",
-            original_filename="article-direction-area.csv",
-            uploaded_filename="492f15d8-45e4-427e-bde0-f60d69889f40",
+            original_filename=filename,
+            uploaded_filename=uploaded_filename,
         ).model_dump(),
     )
     db_session = database.session_maker()
@@ -67,12 +77,9 @@ def test_check_datafile(
     destination_organisation_csv = os.path.join(
         mock_directories.CACHE_DIR, "organisation.csv"
     )
-    print("destination_organisation_csv=" + destination_organisation_csv)
+    # print("destination_organisation_csv=" + destination_organisation_csv)
     shutil.copy(source_organisation_csv, destination_organisation_csv)
-    print(
-        "destination_organisation_csv exists? "
-        + str(os.path.exists(destination_organisation_csv))
-    )
+    # print("destination_organisation_csv exists? "+ str(os.path.exists(destination_organisation_csv)))
     mocker.patch(
         "application.core.workflow.fetch_pipeline_csvs",
         side_effect=mock_fetch_pipeline_csvs("article-4-direction-area", request.id),
@@ -85,79 +92,29 @@ def test_check_datafile(
     mock_directories_json = json.dumps(mock_directories_str)
     check_datafile_task = celery_app.register_task(check_datafile)
     check_datafile_task.delay(request.model_dump(), directories=mock_directories_json)
-    _wait_for_request_status(request.id, "COMPLETE")
+    _wait_for_request_status(request.id, expected_status)
 
 
-def test_check_datafile_invalid(
-    mocker,
-    celery_app,
-    celery_worker,
-    s3_bucket,
-    db,
-    mock_directories,
-    mock_fetch_pipeline_csvs,
-    test_data_dir,
-):
-    """
-    This function tests the check_datafile task for an invalid file validation.
-
-    Args:
-        mocker: Mocking framework for Python.
-        celery_app: Celery application.
-        celery_worker: Celery worker.
-        s3_bucket: S3 bucket.
-        db: Database.
-        mock_directories: Mocked directories.
-        mock_fetch_pipeline_csvs: Mock function for fetching pipeline CSVs.
-        test_data_dir: Directory containing test data.
-    """
-    request_model = models.Request(
-        type=schemas.RequestTypeEnum.check_file,
-        created=datetime.datetime.now(),
-        modified=datetime.datetime.now(),
-        status="NEW",
-        params=schemas.CheckFileParams(
-            collection="article-4-direction",
-            dataset="article-4-direction-area",
-            original_filename="invalid.csv",
-            uploaded_filename="invalid",
-        ).model_dump(),
-    )
-    db_session = database.session_maker()
-    with db_session() as session:
-        session.add(request_model)
-        session.commit()
-        session.refresh(request_model)
-    request = schemas.Request(
-        id=request_model.id,
-        type=request_model.type,
-        status=request_model.status,
-        created=request_model.created,
-        modified=request_model.modified,
-        params=request_model.params,
-        response=None,
-    )
-    source_organisation_csv = f"{test_data_dir}/csvs/organisation.csv"
-    destination_organisation_csv = os.path.join(
-        mock_directories.CACHE_DIR, "organisation.csv"
-    )
-    shutil.copy(source_organisation_csv, destination_organisation_csv)
-
-    mocker.patch(
-        "application.core.workflow.fetch_pipeline_csvs",
-        side_effect=mock_fetch_pipeline_csvs("article-4-direction-area", request.id),
-    )
-    # Convert mock directory paths to strings
-    mock_directories_str = {
-        key: str(path) for key, path in mock_directories._asdict().items()
-    }
-
-    mock_directories_json = json.dumps(mock_directories_str)
-    check_datafile_task = celery_app.register_task(check_datafile)
-    check_datafile_task.delay(request.model_dump(), directories=mock_directories_json)
-    _wait_for_request_status(request.id, "FAILED")
-
-
+@pytest.mark.parametrize(
+    "test_name, url, get_request_return_value, expected_status, mock_response",
+    [
+        (
+            "valid_url",
+            "exampleurl.csv",
+            (None,
+             '{"type":"FeatureCollection","properties":{"exceededTransferLimit":true}, "features":[{"type":"Feature","id":1,"geometry":{"type":"Point", "coordinates":[-1.59153574212325,54.9392094142866]}, "properties": {"reference": "CA01","name": "Ashleworth Conservation Area"}}]}'.encode("utf-8")),
+            "COMPLETE",
+            True,
+        ),
+        (
+            "invalid_url",
+            "exampleurl.csv",
+            ('{"status": "404", "message": "Unable to process"}', None),
+            "FAILED",
+            False,
+        ),
+    ],
+)
 def test_check_datafile_url(
     mocker,
     celery_app,
@@ -167,9 +124,14 @@ def test_check_datafile_url(
     mock_directories,
     mock_fetch_pipeline_csvs,
     test_data_dir,
+    test_name,
+    url,
+    get_request_return_value,
+    expected_status,
+    mock_response,
 ):
     """
-    This function tests the check_datafile task for a valid URL validation.
+    This function tests the check_datafile task for URL validation.
 
     Args:
         mocker: Mocking framework for Python.
@@ -180,6 +142,11 @@ def test_check_datafile_url(
         mock_directories: Mocked directories.
         mock_fetch_pipeline_csvs: Mock function for fetching pipeline CSVs.
         test_data_dir: Directory containing test data.
+        test_name: Name of the test case.
+        url: The URL to validate.
+        get_request_return_value: The return value of the mocked get_request function.
+        expected_status: The expected status of the request.
+        mock_response: determine if mock fetch_pipeline_csvs should be called.
     """
     request_model = models.Request(
         type=schemas.RequestTypeEnum.check_url,
@@ -189,7 +156,7 @@ def test_check_datafile_url(
         params=schemas.CheckUrlParams(
             collection="article-4-direction",
             dataset="article-4-direction-area",
-            url="exampleurl.csv",
+            url=url,
         ).model_dump(),
     )
     db_session = database.session_maker()
@@ -206,111 +173,42 @@ def test_check_datafile_url(
         params=request_model.params,
         response=None,
     )
-    source_organisation_csv = f"{test_data_dir}/csvs/organisation.csv"
-    destination_organisation_csv = os.path.join(
-        mock_directories.CACHE_DIR, "organisation.csv"
-    )
-    print("destination_organisation_csv=" + destination_organisation_csv)
-    shutil.copy(source_organisation_csv, destination_organisation_csv)
-    print(
-        "destination_organisation_csv exists? "
-        + str(os.path.exists(destination_organisation_csv))
-    )
 
-    mocker.patch(
-        "application.core.workflow.fetch_pipeline_csvs",
-        side_effect=mock_fetch_pipeline_csvs("article-4-direction-area", request.id),
-    )
-    # Convert mock directory paths to strings
+    if mock_response:
+        source_organisation_csv = f"{test_data_dir}/csvs/organisation.csv"
+        destination_organisation_csv = os.path.join(
+            mock_directories.CACHE_DIR, "organisation.csv"
+        )
+        print("destination_organisation_csv=" + destination_organisation_csv)
+        shutil.copy(source_organisation_csv, destination_organisation_csv)
+        print(
+            "destination_organisation_csv exists? "
+            + str(os.path.exists(destination_organisation_csv))
+        )
+
+        mocker.patch(
+            "application.core.workflow.fetch_pipeline_csvs",
+            side_effect=mock_fetch_pipeline_csvs("article-4-direction-area", request.id),
+        )
+
     mock_directories_str = {
         key: str(path) for key, path in mock_directories._asdict().items()
     }
 
-    content = '{"type":"FeatureCollection","properties":{"exceededTransferLimit":true}, \
-    "features":[{"type":"Feature","id":1,"geometry":{"type":"Point", \
-    "coordinates":[-1.59153574212325,54.9392094142866]}, \
-    "properties": {"reference": "CA01","name": "Ashleworth Conservation Area"}}]}'
-
-    # Encode the JSON string to bytes using UTF-8 encoding
-    content_bytes = content.encode("utf-8")
-    # Mock the result of utils.get_request
     mocker.patch(
-        "application.core.utils.get_request", return_value=(None, content_bytes)
+        "application.core.utils.get_request", return_value=get_request_return_value
     )
 
     mock_directories_json = json.dumps(mock_directories_str)
     check_datafile_task = celery_app.register_task(check_datafile)
     check_datafile_task.delay(request.model_dump(), directories=mock_directories_json)
-    _wait_for_request_status(request.id, "COMPLETE")
-
-
-def test_check_datafile_url_invalid(
-    mocker,
-    celery_app,
-    celery_worker,
-    s3_bucket,
-    db,
-    mock_directories,
-    mock_fetch_pipeline_csvs,
-    test_data_dir,
-):
-    """
-    This function tests the check_datafile task for an invalid URL validation.
-
-    Args:
-        mocker: Mocking framework for Python.
-        celery_app: Celery application.
-        celery_worker: Celery worker.
-        s3_bucket: S3 bucket.
-        db: Database.
-        mock_directories: Mocked directories.
-        mock_fetch_pipeline_csvs: Mock function for fetching pipeline CSVs.
-        test_data_dir: Directory containing test data.
-    """
-    request_model = models.Request(
-        type=schemas.RequestTypeEnum.check_url,
-        created=datetime.datetime.now(),
-        modified=datetime.datetime.now(),
-        status="NEW",
-        params=schemas.CheckUrlParams(
-            collection="article-4-direction",
-            dataset="article-4-direction-area",
-            url="exampleurl.csv",
-        ).model_dump(),
-    )
-    db_session = database.session_maker()
-    with db_session() as session:
-        session.add(request_model)
-        session.commit()
-        session.refresh(request_model)
-    request = schemas.Request(
-        id=request_model.id,
-        type=request_model.type,
-        status=request_model.status,
-        created=request_model.created,
-        modified=request_model.modified,
-        params=request_model.params,
-        response=None,
-    )
-    # Convert mock directory paths to strings
-    mock_directories_str = {
-        key: str(path) for key, path in mock_directories._asdict().items()
-    }
-
-    log = '{"status": "404", "message": "Unable to process"}'
-
-    # Mock the result of utils.get_request
-    mocker.patch("application.core.utils.get_request", return_value=(log, None))
-
-    mock_directories_json = json.dumps(mock_directories_str)
-    check_datafile_task = celery_app.register_task(check_datafile)
-    check_datafile_task.delay(request.model_dump(), directories=mock_directories_json)
-    _wait_for_request_status(request.id, "FAILED")
+    _wait_for_request_status(request.id, expected_status)
 
 
 def _wait_for_request_status(
     request_id, expected_status, timeout_seconds=10, interval_seconds=1
 ):
+    # print("ACTUAL STATUS::::::", expected_status)
     seconds_waited = 0
     actual_status = "UNKNOWN"
     while seconds_waited <= timeout_seconds:
