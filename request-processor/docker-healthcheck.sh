fe294d775f04c36465badf06a34594c56e06c362
Samriti Sadhu <ssadhu@scottlogic.com>
2024-11-25 15:24:21 +0000

    adding back procps for pgrep

--- request-processor/docker-healthcheck.sh
+++ request-processor/docker-healthcheck.sh
@@ -1,33 +1,30 @@
 # Checks health by checking worker process is running along with connectivity checks of SQS and Postgres
 
-# Prevent exit on error code
-set -e
-
 # Check Celery process is running
-pgrep celery
-#pgrep celery > /dev/null 2> /dev/null
-
+pgrep celery > /dev/null 2> /dev/null
+exit_code=$?
 
 # Check SQS queue exists
-aws sqs get-queue-url --queue-name celery
-#if aws sqs get-queue-url --queue-name celery > /dev/null 2> /dev/null
-#then
-#  sqs_status="HEALTHY"
-#else
-#  sqs_status="HEALTHY"; #Passing the status as healthy in all scenarios
-#fi
+if aws sqs get-queue-url --queue-name celery > /dev/null 2> /dev/null
+then
+  sqs_status="HEALTHY"
+else
+  sqs_status="UNHEALTHY";
+  exit_code=1;
+fi
 
 # Check Postgres DB is ready
-pg_isready -d "$DATABASE_URL"
-#if pg_isready -d "$DATABASE_URL" > /dev/null 2> /dev/null
-#then
-#  pg_status="HEALTHY"
-#else
-#  pg_status="UNHEALTHY";
-#fi
+if pg_isready -d "$DATABASE_URL" > /dev/null 2> /dev/null
+then
+  pg_status="HEALTHY"
+else
+  pg_status="UNHEALTHY";
+  exit_code=1;
+fi
 
 # Provide JSON output
-#jq ".version=\"$GIT_COMMIT\" | (.dependencies[] | select(.name==\"request-db\")).status=\"$pg_status\" | (.dependencies[] | select(.name==\"sqs\")).status=\"$sqs_status\"" healthcheck-output-template.json
+jq ".version=\"$GIT_COMMIT\" | (.dependencies[] | select(.name==\"request-db\")).status=\"$pg_status\" | (.dependencies[] | select(.name==\"sqs\")).status=\"$sqs_status\"" healthcheck-output-template.json
 
+exit $exit_code
 
-# Note use of > /dev/null 2> /dev/null which suppresses stout and stderr
+# Note use of  > /dev/null 2> /dev/null which suppresses stout and stderr
