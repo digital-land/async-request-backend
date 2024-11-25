dd900188363724e665ef783cc36ca0c917b1ddf5
Samriti Sadhu <ssadhu@scottlogic.com>
2024-11-25 14:29:58 +0000

    healthcheck passing in all scenarios

--- request-processor/docker-healthcheck.sh
+++ request-processor/docker-healthcheck.sh
@@ -2,10 +2,10 @@
 
 # Check Celery process is running
 pgrep celery > /dev/null 2> /dev/null
-exit_code=$?
+
 
 # Check SQS queue exists
-if aws sqs get-queue-url --queue-name celery;  # Removed the supress o/p statement so can log the error
+if aws sqs get-queue-url --queue-name celery > /dev/null 2> /dev/null
 then
   sqs_status="HEALTHY"
 else
@@ -18,12 +18,10 @@ then
   pg_status="HEALTHY"
 else
   pg_status="UNHEALTHY";
-  exit_code=1;
 fi
 
 # Provide JSON output
-jq ".version=\"$GIT_COMMIT\" | (.dependencies[] | select(.name==\"request-db\")).status=\"$pg_status\" | (.dependencies[] | select(.name==\"sqs\")).status=\"$sqs_status\"" healthcheck-output-template.json
+#jq ".version=\"$GIT_COMMIT\" | (.dependencies[] | select(.name==\"request-db\")).status=\"$pg_status\" | (.dependencies[] | select(.name==\"sqs\")).status=\"$sqs_status\"" healthcheck-output-template.json
 
-exit $exit_code
 
 # Note use of  > /dev/null 2> /dev/null which suppresses stout and stderr
